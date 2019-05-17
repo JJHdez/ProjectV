521168a5941ba5a5c3470bcac193cb4153761b8a
José Hernández <jhbez@outlook.com>
2019-05-17 18:31:54 -0500

    add docker-compose

---
+++ tools/db/v1.0.0.sql
@@ -0,0 +1,5 @@
+--DROP DATABASE IF EXISTS focus;
+--CREATE DATABASE focus;
+--CREATE USER focus WITH ENCRYPTED PASSWORD 'focus';
+--                               dbname to username
+--GRANT ALL PRIVILEGES ON DATABASE focus TO focus;
